18a3e704adf27fd8f5eb125a34f3720841f13d57
azeizi09 <alexzeizi1@gmail.com>
2017-02-07 12:03:38 -0600

    Update main.cpp

--- assignments/program_1/main.cpp
+++ assignments/program_1/main.cpp
@@ -7,37 +7,6 @@
 * @Semester: Spring 2017
 * @Date: 02 06 2017
 */
-/**
-
-* TXT Image Manipulation Starter
-
-* 
-
-* This code is a simple way to read in color information stored in a space
-
-* delimited txt format. The expected file format is:
-
-*                ---------------------------
-
-*                | width height            |
-
-*                | R G B R G B R G B R G B |
-
-*                | R G B R G B R G B R G B |
-
-*                | R G B R G B R G B R G B |
-
-*                | R G B R G B R G B R G B |
-
-*                | R G B R G B R G B R G B |
-
-*                | R G B R G B R G B R G B |
-
-*                ---------------------------
-
-* So a 10x10 img would have 11 total rows, 10 rows of data, with 30 values in row.
-
-*/
 
 #include<iostream>
 #include<fstream>
@@ -45,9 +14,7 @@
 using namespace std;
 
 /**
-
 Structure to hold an rgb value
-
 */
 struct rgb
 {
@@ -67,9 +34,24 @@ struct rgb
 * @Returns:
 *    void
 */
-void flipVert(rgb** image,int width,int height)
+void flipVert(rgb** image, int width, int height)
 {
-    // your code
+    int Hold;
+    for(int i = 0; i < height/2; i++)
+    {
+      for(int j = 0; j < width; j++)
+      {
+        Hold = image[i][j].r;
+        image[i][j].r = image[height-1-i][j].r;
+        image[height-1-i][j].r = Hold;
+        Hold = image[i][j].g;
+        image[i][j].g = image[height-1-i][j].g;
+        image[height-1-i][j].g = Hold;
+        Hold = image[i][j].b;
+        image[i][j].b = image[height-1-i][j].b;
+        image[height-1-i][j].b = Hold;
+      }
+    }
 }
 
 /**
@@ -83,9 +65,24 @@ void flipVert(rgb** image,int width,int height)
 * @Returns:
 *    void
 */
-void flipHorz(rgb** image,int width,int height)
+void flipHorz(rgb** image, int width, int height)
 {
-    // your code
+    rgb Hold;
+    for(int i = 0; i < height; i++)
+    {
+      for(int j = 0; j < width/2; j++)
+      {
+        Hold.r = image[i][j].r;
+        Hold.g = image[i][j].g;
+        Hold.b = image[i][j].b;
+        image[i][j].r = image[i][width-j-1].r;
+        image[i][j].g = image[i][width-j-1].g;
+        image[i][j].b = image[i][width-j-1].b;
+        image[i][width-j-1].r = Hold.r;
+        image[i][width-j-1].g = Hold.g;
+        image[i][width-j-1].b = Hold.b;
+;      }
+    }
 }
 
 /**
@@ -99,9 +96,19 @@ void flipHorz(rgb** image,int width,int height)
 * @Returns:
 *    void
 */
-void grayScale(rgb** image,int width,int height)
+void grayScale(rgb** image, int width, int height)
 {
-    // your code
+    int Hold;
+    for(int i = 0; i < height; i++)
+    {
+      for(int j = 0; j < width; j++)
+      {
+        Hold = (image[i][j].r + image[i][j].g + image[i][j].b) / 3;
+        image[i][j].r = Hold;
+        image[i][j].g = Hold;
+        image[i][j].b = Hold;
+      }
+    }
 }
 
 /**
@@ -116,44 +123,17 @@ void grayScale(rgb** image,int width,int height)
 * @Returns:
 *    void
 */
-void changeColor(rgb** image,int width, int height, rgb newColor)
+void changeColor(rgb** image, int width, int height, rgb newColor)
 {
-   // loop through image
-   // assign newColor to each location in array
-}
-
-/**
-* @FunctionName: readImage
-* @Description: 
-*     Loops through a file and reads in RGB values.
-* @Params:
-*    rgb** image - 2D array holding rgb values
-*    int width - width of image
-*    int height - height of image
-*    ofstream ofile - output file
-* @Returns:
-*    void
-*/
-void readImage(rgb** image,int &width,int &height,ofstream &ofile)
-{
-    // Hmmm, what's up with the width and height references
-}
-
-/**
-* @FunctionName: saveImage
-* @Description: 
-*     Loops through a file and stores the image through RGB values.
-* @Params:
-*    rgb** image - 2D array holding rgb values
-*    int width - width of image
-*    int height - height of image
-*    ofstream ofile - output file
-* @Returns:
-*    void
-*/
-void saveImage(rgb** image,int width,int height,ofstream &ofile)
-{
-    // your code
+   for(int i = 0; i < height; i++)
+   {
+     for(int j = 0; j < width; j++)
+     {
+       image[i][j].r = newColor.r;
+       image[i][j].g = newColor.g;
+       image[i][j].b = newColor.b;
+     }
+   }
 }
 
 int main()
@@ -165,11 +145,12 @@ int main()
 
     int width;               //width of image
     int height;              //height of image
-
+    int Option;
+    
     rgb **imgArray;         //Pointer var for our 2D array because we         
                             //don't know how big the image will be
                             
-    ifile>>width>>height;   //Read in width and height from top of input file
+    ifile >> width >> height;   //Read in width and height from top of input file
 
                             //We need this so we can make the array the right 
                             //size. After we get these two values, we can
@@ -177,32 +158,75 @@ int main()
 
     imgArray = new rgb*[height];    //This array points to every row
 
-    for(int i=0;i<height;i++)
+    for(int i = 0; i < height; i++)
     {
         imgArray[i] = new rgb[width]; //Now allocate each row of rgb's
     }
 
     //Read the color data in from our txt file
-    for(int i=0;i<height;i++)
+    for(int i = 0; i < height; i++)
     {
-        for(int j=0;j<width;j++)
+        for(int j = 0; j < width; j++)
         {
             ifile>>imgArray[i][j].r>>imgArray[i][j].g>>imgArray[i][j].b;            
         }
     }
 
-    //We could make any changes we want to the color image here
+    cout << "Please choose an option.\n";
+    cout << "1. Flip vertically.\n";
+    cout << "2. Flip horizontally.\n";
+    cout << "3. Grayscale image.\n";
+    cin >> Option;
 
-    //Write out our color data to a new file
-    ofile<<width<<" "<<height<<endl;
-
-    for(int i=0;i<height;i++)
+    if (Option == 1)
     {
-        for(int j=0;j<width;j++)
+      flipVert(imgArray, width, height);
+      
+      //Write out our color data to a new file
+      ofile<<width<<" "<<height<<endl;
+
+      for(int i = 0; i < height; i++)
+      {
+        for(int j = 0; j < width; j++)
         {
             ofile<<imgArray[i][j].r<<" "<<imgArray[i][j].g<<" "<<imgArray[i][j].b<<" ";
         }
         ofile<<endl;
+      }
+    }
+    
+    else if (Option == 2)
+    {
+      flipHorz(imgArray, width, height);
+      
+      //Write out our color data to a new file
+      ofile<<width<<" "<<height<<endl;
+
+      for(int i = 0; i < height; i++)
+      {
+        for(int j = 0; j < width; j++)
+        {
+            ofile<<imgArray[i][j].r<<" "<<imgArray[i][j].g<<" "<<imgArray[i][j].b<<" ";
+        }
+        ofile<<endl;
+      }
+    }
+    
+    else if (Option == 3)
+    {
+      grayScale(imgArray, width, height);
+      
+      //Write out our color data to a new file
+      ofile<<width<<" "<<height<<endl;
+
+      for(int i = 0; i < height; i++)
+      {
+        for(int j = 0; j < width; j++)
+        {
+            ofile<<imgArray[i][j].r<<" "<<imgArray[i][j].g<<" "<<imgArray[i][j].b<<" ";
+        }
+        ofile<<endl;
+      }
     }
 
   return 0;
